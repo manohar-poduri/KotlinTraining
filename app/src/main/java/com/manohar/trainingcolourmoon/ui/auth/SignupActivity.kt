152d94314eb0da64c5b458d5abbce53bb26f4b80
Manohar - IT man <poduri.manohar46@gmal.com>
2021-07-14 12:10:38 +0530

    Changes

---
+++ app/src/main/java/com/manohar/trainingcolourmoon/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.manohar.trainingcolourmoon.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manohar.trainingcolourmoon.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
